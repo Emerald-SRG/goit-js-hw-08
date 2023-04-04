410f610cf6a82f46d418b4cee33f0983476614ef
Emerald-SRG <97896014+Emerald-SRG@users.noreply.github.com>
2023-04-04 21:10:26 +0300

    add video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const docFrame = document.querySelector('iframe');
+const player = new Player(docFrame);
+player.on('timeupdate', throttle(timeUpdate, 1000));
+function timeUpdate(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+const getTime = (localStorage.getItem('videoplayer-current-time'));
+if (getTime !== null) {
+  player.setCurrentTime(getTime)
+};
